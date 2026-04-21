bcd390739b4b6c5baa058f039f30f3671748de4e
sup <ivannamber31@gmail.com>
2026-04-21 17:09:28 +0300

    Added route to login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainPage } from "./pages/MainPage";
 import { Page1 } from "./pages/Page1";
 import { Page2 } from "./pages/Page2";
-import RegisterPage from "./pages/RegisterPage/RegisterPage"; 
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/page1" element={<Page1 />} />
           <Route path="/page2" element={<Page2 />} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
