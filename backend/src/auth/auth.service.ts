ae75b334c0cf35fe05ceea394410abbd9170afef
AndriyPastukh <vasylpaspuh2@gmail.com>
2026-04-22 15:36:00 +0300

    feat(auth): implement JWT login and registration logic [KAN-59]

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,14 @@
 // auth/auth.service.ts
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 import { UsersService } from '../users/users.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -11,21 +17,54 @@ export class AuthService {
   async register(dto: RegisterDto) {
     const { email, password } = dto;
 
-    // 1. перевірка унікальності
     const existingUser = await this.usersService.findByEmail(email);
 
     if (existingUser) {
       throw new ConflictException('Email already exists');
     }
 
-    // 2. хешування
     const passwordHash = await bcrypt.hash(password, 10);
 
-    // 3. створення
     await this.usersService.create(email, passwordHash);
 
     return {
       message: 'User created successfully',
+    };
+  }
+
+  async login(dto: LoginDto) {
+    const { email, password } = dto;
+
+    // 1. Пошук користувача
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // 2. Перевірка паролю
+    const isPasswordValid = await bcrypt.compare(password, user.passwordHash);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    // 3. Генерація токенів
+    const accessToken = jwt.sign(
+      { sub: user.id, email: user.email },
+      process.env.JWT_ACCESS_SECRET!,
+      { expiresIn: '15m' },
+    );
+
+    const refreshToken = jwt.sign(
+      { sub: user.id, email: user.email },
+      process.env.JWT_REFRESH_SECRET!,
+      { expiresIn: '30d' },
+    );
+
+    return {
+      accessToken,
+      refreshToken,
     };
   }
 }
