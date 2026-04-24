ea87bae0633272912d0c07045148228984f0e618
MK-explorer <argneo0@gmail.com>
2026-04-25 00:36:11 +0300

    removed unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,26 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Імпортуємо ваші сторінки
 import { MainPage } from "./pages/MainPage/MainPage.tsx";
-import { Page1 } from "./pages/Page1";
-import { Page2 } from "./pages/Page2";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import LoginPage from "./pages/LoginPage/LoginPage.tsx";
 import { Header } from "./components/Header/Header.tsx";
-import {Footer} from "./components/Footer/Footer.tsx"
+import { Footer } from "./components/Footer/Footer.tsx"
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/page1" element={<Page1 />} />
-          <Route path="/page2" element={<Page2 />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </BrowserRouter>
   );
